97a82b9f28a96ad256d8ab34a68bfbcebb166362
Tim Wang <tianxun.wang1@nhs.net>
2022-11-07 16:50:37 +0000

    added script to update fonts

--- setup.sh
+++ setup.sh
@@ -64,24 +64,5 @@ fi
 if [[ $ADD_FONTS = 1 ]]
 then
   _display_message "ADDING FONTS"
-  mkdir -p ~/.fonts
-  pushd ~/.fonts
-  curl -fLo "JetBrains Mono Medium Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Medium/complete/JetBrains%20Mono%20Medium%20Nerd%20Font%20Complete%20Mono.ttf
-  curl -fLo "JetBrainsMono-Bold.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-Bold.ttf
-  curl -fLo "JetBrainsMono-BoldItalic.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-BoldItalic.ttf
-  curl -fLo "JetBrainsMono-ExtraBold.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-ExtraBold.ttf
-  curl -fLo "JetBrainsMono-ExtraBoldItalic.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-ExtraBoldItalic.ttf
-  curl -fLo "JetBrainsMono-ExtraLight.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-ExtraLight.ttf
-  curl -fLo "JetBrainsMono-ExtraLightItalic.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-ExtraLightItalic.ttf
-  curl -fLo "JetBrainsMono-Italic.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-Italic.ttf
-  curl -fLo "JetBrainsMono-Light.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-Light.ttf
-  curl -fLo "JetBrainsMono-LightItalic.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-LightItalic.ttf
-  curl -fLo "JetBrainsMono-Medium.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-Medium.ttf
-  curl -fLo "JetBrainsMono-MediumItalic.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-MediumItalic.ttf
-  curl -fLo "JetBrainsMono-Regular.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-Regular.ttf
-  curl -fLo "JetBrainsMono-SemiBold.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-SemiBold.ttf
-  curl -fLo "JetBrainsMono-SemiBoldItalic.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-SemiBoldItalic.ttf
-  curl -fLo "JetBrainsMono-Thin.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-Thin.ttf
-  curl -fLo "JetBrainsMono-ThinItalic.ttf" https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-ThinItalic.ttf
-  popd
+  cp -r ./.fonts ~
 fi
