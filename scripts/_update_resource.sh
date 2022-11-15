ba1ddd79ad46848954df970d3b3f0c7186ce6d0a
Tim <tianxun.wang1@nhs.net>
2022-11-15 10:59:35 +0000

    added script to update themes

--- scripts/_update_resource.sh
+++ scripts/_update_resource.sh
@@ -15,8 +15,40 @@ function _update() {
 
   case $command in
   "fonts") _update_fonts ;;
+  "themes") _update_themes ;;
   *) _update_help ;;
   esac
+}
+
+function _update_themes() {
+  _display_message "UPDATING THEMES"
+
+  echo "Fetching latest release"
+  local latest_release
+  latest_release=$(curl -s https://api.github.com/repos/EliverLara/Nordic/releases/latest | jq -r .tag_name)
+
+  echo "Latest release is: ${latest_release}"
+
+
+  echo "Updating KDE themes"
+  local nordic_git_dir="$TMP_DIR/nordic-git"
+  rm -rf "$TMP_DIR"
+
+  git clone --branch "$latest_release" git@github.com:EliverLara/Nordic.git "$nordic_git_dir"
+  rm -rf "$AURORAE_DIR"
+  rm -rf "$COLORSCHEMES_DIR"
+  mkdir -p "$AURORAE_DIR"
+  mkdir -p "$COLORSCHEMES_DIR"
+  cp -r "${nordic_git_dir}/kde/aurorae/." "$AURORAE_DIR/themes/"
+  cp -r "${nordic_git_dir}/kde/colorschemes/." "$COLORSCHEMES_DIR/"
+
+  echo "Updating GTK Themes"
+  wget -q "https://github.com/EliverLara/Nordic/releases/download/${latest_release}/Nordic-darker.tar.xz" -P "$TMP_DIR"
+  rm -rf "$THEMES_DIR"
+  mkdir -p "$THEMES_DIR"
+  tar -xf "$TMP_DIR/Nordic-darker.tar.xz" -C "$THEMES_DIR"
+
+  echo "Complete"
 }
 
 function _update_fonts() {
