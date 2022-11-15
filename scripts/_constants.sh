e77e82d5f1e7be5f9f4c7f85765675ae35a10ff1
Tim <tianxun.wang1@nhs.net>
2022-11-15 15:44:20 +0000

    added arc themes

--- scripts/_constants.sh
+++ scripts/_constants.sh
@@ -10,6 +10,7 @@ ZSH_CONFIG="${REPO_ROOT}/.zshrc"
 TMP_DIR="${REPO_ROOT}/tmp"
 AURORAE_DIR="${LOCALS_DIR}/share/aurorae"
 COLORSCHEMES_DIR="${LOCALS_DIR}/share/color-schemes"
+LOOK_AND_FEEL_DIR="${LOCALS_DIR}/share/plasma/look-and-feel"
 
 NERD_FONTS_GITHUB="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Medium/complete"
 JETBRAINS_MONO_FONTS_GITHUB="https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf"
--- scripts/_update_resource.sh
+++ scripts/_update_resource.sh
@@ -20,33 +20,60 @@ function _update() {
   esac
 }
 
-function _update_themes() {
-  _display_message "UPDATING THEMES"
-
-  echo "Fetching latest release"
+function _update_nordic_theme() {
+  local git_dir="$TMP_DIR/nordic-git"
   local latest_release
-  latest_release=$(curl -s https://api.github.com/repos/EliverLara/Nordic/releases/latest | jq -r .tag_name)
+
+  echo "Fetching latest release for: EliverLara/Nordic"
+
+  latest_release=$(curl -s "https://api.github.com/repos/EliverLara/Nordic/releases/latest" | jq -r .tag_name)
 
   echo "Latest release is: ${latest_release}"
 
-
   echo "Updating KDE themes"
-  local nordic_git_dir="$TMP_DIR/nordic-git"
-  rm -rf "$TMP_DIR"
 
-  git clone --branch "$latest_release" git@github.com:EliverLara/Nordic.git "$nordic_git_dir"
-  rm -rf "$AURORAE_DIR"
-  rm -rf "$COLORSCHEMES_DIR"
-  mkdir -p "$AURORAE_DIR"
-  mkdir -p "$COLORSCHEMES_DIR"
-  cp -r "${nordic_git_dir}/kde/aurorae/." "$AURORAE_DIR/themes/"
-  cp -r "${nordic_git_dir}/kde/colorschemes/." "$COLORSCHEMES_DIR/"
+  git clone --branch "$latest_release" https://github.com/EliverLara/Nordic.git "$git_dir"
+  cp -r "${git_dir}/kde/aurorae/." "$AURORAE_DIR/themes/"
+  cp -r "${git_dir}/kde/colorschemes/." "$COLORSCHEMES_DIR/"
 
   echo "Updating GTK Themes"
   wget -q "https://github.com/EliverLara/Nordic/releases/download/${latest_release}/Nordic-darker.tar.xz" -P "$TMP_DIR"
+  tar -xf "$TMP_DIR/Nordic-darker.tar.xz" -C "$THEMES_DIR"
+}
+
+function _update_arc_theme() {
+  local git_dir="$TMP_DIR/arc-git"
+  local latest_release
+
+  echo "Fetching latest release for: PapirusDevelopmentTeam/arc-kde"
+
+  latest_release=$(curl -s "https://api.github.com/repos/PapirusDevelopmentTeam/arc-kde/releases/latest" | jq -r .tag_name)
+
+  echo "Latest release is: ${latest_release}"
+
+  echo "Updating KDE themes"
+
+  git clone --branch "$latest_release" https://github.com/PapirusDevelopmentTeam/arc-kde.git "$git_dir"
+  cp -r "${git_dir}/aurorae/themes/." "$AURORAE_DIR/themes/"
+  cp -r "${git_dir}/color-schemes/." "$COLORSCHEMES_DIR/"
+  cp -r "${git_dir}/plasma/look-and-feel/." "$LOOK_AND_FEEL_DIR/"
+}
+
+function _update_themes() {
+  _display_message "UPDATING THEMES"
+  rm -rf "$TMP_DIR"
+
+  rm -rf "$AURORAE_DIR"
+  rm -rf "$COLORSCHEMES_DIR"
+  rm -rf "$LOOK_AND_FEEL_DIR"
+  mkdir -p "$AURORAE_DIR"
+  mkdir -p "$COLORSCHEMES_DIR"
+  mkdir -p "$LOOK_AND_FEEL_DIR"
   rm -rf "$THEMES_DIR"
   mkdir -p "$THEMES_DIR"
-  tar -xf "$TMP_DIR/Nordic-darker.tar.xz" -C "$THEMES_DIR"
+
+  _update_nordic_theme
+  _update_arc_theme
 
   echo "Complete"
 }
